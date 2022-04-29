7e6bcadb8613b7a77c37a49c725c3672f6c36d4c
Dmitry <dp_140482@mail.ru>
2022-04-29 19:51:37 +0300

    Откл. комментариев

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -46,12 +46,6 @@ export default new Vuex.Store({
     },
     setPersonList(state, payload) {
       state.personList = payload
-    },
-    setCommentList (state, payload) {
-      state.commentList = payload
-    },
-    addComment (state, payload) {
-      state.commentList = [payload, ...state.commentList]
     }
   },
   getters: {
@@ -64,7 +58,6 @@ export default new Vuex.Store({
     getVideos: state => state.videos,
     getGenres: state => state.genres,
     getPersonsList: state => state.personList,
-    getCommentList: state => state.commentList,
   },
   actions: {
     fetchRatingItems({ commit }) {
@@ -102,62 +95,6 @@ export default new Vuex.Store({
     },
     fetchDirectors({ commit }, payload) {
       commit('setPersonList', [...this.state.personList, ...payload])
-    },
-    fetchComments ({ commit }) {
-      return commit('setCommentList', [
-        {
-          filmRoute: 'avatar',
-          username: 'Гость 1',
-          comment: 'Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1,Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1, Текст коментария 1.',
-          datetime: '18.04.2022, 18:30',
-          id: '1'
-        },
-        {
-          filmRoute: 'hurrypotter-azkaban',
-          username: 'Гость 8',
-          comment: 'Текст коментария 2, Текст коментария 2, Текст коментария 2, Текст коментария 2',
-          datetime: '18.04.2022, 18:30',
-          id: '2'
-        },
-        {
-          filmRoute: 'avatar',
-          username: 'Гость 2',
-          comment: 'Текст коментария3',
-          datetime: '18.04.2022, 19:00',
-          id: '3'
-        },
-        {
-          filmRoute: 'snowwhite',
-          username: 'Гость 2',
-          comment: 'Текст коментария 4, Текст коментария, Текст коментария, Текст коментария, Текст коментария',
-          datetime: '18.04.2022, 18:30',
-          id: '4'
-        },
-        {
-          filmRoute: 'matrix',
-          username: 'Гость 4',
-          comment: 'Текст коментария 5, Текст коментария, Текст коментария, Текст коментария, Текст коментария, Текст коментария, Текст коментария',
-          datetime: '18.04.2022, 19:00',
-          id: '5'
-        },
-        {
-          filmRoute: 'everest',
-          username: 'Гость 2',
-          comment: 'Текст коментария 6',
-          datetime: '18.04.2022, 19:00',
-          id: '6'
-        },
-        {
-          filmRoute: 'hurrypotter-azkaban',
-          username: 'Гость 1',
-          comment: 'Текст коментария 7',
-          datetime: '18.04.2022, 19:00',
-          id: '7'
-        }
-      ])
-    },
-    fetchAddComment ({ commit }, payload) {
-      commit('addComment', payload)
     },
     fetchNews ({ commit }) {
       return axios.get('api/news').then(result=>{
