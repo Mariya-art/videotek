e09050c04569a0e342cea93b6c6678e0bedf04ec
Dmitry <dp_140482@mail.ru>
2022-05-12 23:13:12 +0300

    Размер страницы
    Размер страницы по высоте содержимого

--- resources/js/components/Films.vue
+++ resources/js/components/Films.vue
@@ -150,7 +150,6 @@ export default {
 .films-list {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
   grid-column-gap: 5%;
   grid-row-gap: 10px;
 }
